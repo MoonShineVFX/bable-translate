b884e81fc115fff09dee5e9cd260e7db5b7440be
blueoick <jouabc3@gmail.com>
2023-09-07 10:32:25 +0800

    add speech to speech

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import requests,json
-from flask import Flask, request, abort,render_template,jsonify,current_app
+from flask import Flask, request, abort,render_template,jsonify,current_app,send_file
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -7,10 +7,11 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MemberJoinedEvent,MessageEvent, TextMessage, TextSendMessage,TemplateSendMessage, ImageCarouselTemplate ,ImageCarouselColumn ,MessageAction,URIAction,PostbackEvent,ButtonsTemplate,PostbackTemplateAction,CarouselTemplate,CarouselColumn,FlexSendMessage,ImageSendMessage,VideoSendMessage,PostbackAction,AudioMessage)
-import logging,os
+    MemberJoinedEvent,MessageEvent, TextMessage, TextSendMessage,TemplateSendMessage, ImageCarouselTemplate ,ImageCarouselColumn ,MessageAction,URIAction,PostbackEvent,ButtonsTemplate,PostbackTemplateAction,CarouselTemplate,CarouselColumn,FlexSendMessage,ImageSendMessage,VideoSendMessage,PostbackAction,AudioMessage,AudioSendMessage)
+import logging,os,time
 import openai
 import base64
+from mutagen.mp3 import MP3
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
@@ -22,9 +23,35 @@ line_bot_api = LineBotApi(os.environ['LINE_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['LINE_ACCESS_SECRET'])
 google_api_key = os.getenv("GOOGLE_TRANSLATION_API_KEY")
 openai.api_key = os.getenv("OPENAI_API_KEY")
+google_speech_api_key = os.getenv("GOOGLE_TEXT_TO_SPEECH_API_KEY")
+app_url = os.getenv("APP_URL")
 
+def text2speech(text,languageCode,file_path):
+    api_endpoint = 'https://texttospeech.googleapis.com/v1/text:synthesize?key=' + google_speech_api_key
+    data = {
+        "input": {
+            "text": text  
+        },
+        "voice": {
+            "languageCode": languageCode,  
+            "ssmlGender": "FEMALE"  
+        },
+        "audioConfig": {
+            "audioEncoding": "MP3",  
+            "sampleRateHertz": 16000  
+        }
+    }
+    response = requests.post(api_endpoint, json=data)
+    response_data = json.loads(response.text)
+    audio_content = response_data["audioContent"]
+    audio_bytes = base64.b64decode(audio_content)
+    with open(file_path, "wb") as audio_file:
+        audio_file.write(audio_bytes)
+    audio = MP3(file_path)
+    duration = audio.info.length
+    return file_path,duration
 
-def openaispeech(filepath):
+def openaispeech2text(filepath):
     audio_file= open(filepath, "rb")
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
     #transcript = openai.Audio.translate("whisper-1", audio_file)
@@ -74,6 +101,14 @@ def callback():
 
     return 'OK'
 
+@app.route('/audio/<filename>')
+def downloadaudio(filename):
+    file_path = filename
+    try:
+        return send_file(file_path, mimetype='audio/mp3')
+    except Exception as e:
+        logger.error(f'e:{e}')
+
 @handler.add(MemberJoinedEvent)
 def handle_membermessage(event):
     if (event.type=="memberJoined"):
@@ -91,16 +126,25 @@ def handle_message(event):
             for chunk in UserSendAudio.iter_content():
                 fd.write(chunk)
         
-        text = openaispeech(path)
+        text = openaispeech2text(path)
         language = detect(text)
         if language == 'zh-CN' or language == 'zh-TW':
             translated_text = googletranslate(language,'en',text)
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=translated_text))
+            file,duration = text2speech(translated_text,'en-US',event.message.id+'_t.mp3')
+            audio_url = f'{app_url}audio/{file}'
+            line_bot_api.reply_message(event.reply_token, AudioSendMessage(original_content_url=audio_url, duration=duration+1000))
         elif language == 'en':
             translated_text = googletranslate(language,'zh-TW',text)
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=translated_text))
-        os.remove(path)
-        
+            file,duration = text2speech(translated_text,'zh-TW',event.message.id+'_t.mp3')
+            audio_url = f'{app_url}audio/{file}'
+            line_bot_api.reply_message(event.reply_token, AudioSendMessage(original_content_url=audio_url, duration=duration*1000))
+        try:
+            os.remove(path)
+            time.sleep(30)
+            os.remove(file)
+        except:
+            logger.info(f'找不到檔案')
+
     elif event.message.type=="text" :
         if event.source.type=="group" or event.source.type=="room" or event.source.type=='user':
             language = detect(event.message.text)
